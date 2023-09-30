46f13bb1a2e63badc7354153ae77b5dd0c4867be
Fabian Villalobos <fabian.villalobos@alumnos.upm.es>
2023-10-01 01:35:29 +0200

    Added dice roll images

--- app/src/main/java/com/example/myapp_2/MainActivity.kt
+++ app/src/main/java/com/example/myapp_2/MainActivity.kt
@@ -3,30 +3,36 @@ package com.example.myapp_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvRollResult: TextView
     private lateinit var btnRoll: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvRollResult = findViewById(R.id.main_tv_rollResult)
         btnRoll = findViewById(R.id.main_btn_roll)
-
         btnRoll.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.main_tv_rollResult)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.main_iv_dice)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
